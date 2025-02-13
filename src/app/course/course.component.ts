325afc528bea438ac27500f23c680c9044fe0bb4
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2025-02-13 12:38:22 +0200

    Single data observable pattern

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,42 +1,49 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
+import {combineLatest, concat, Observable, of} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import {CoursesService} from "../services/courses.service";
+import {LoadingService} from "../loading/loading.service";
+import {map, tap} from "rxjs/operators";
 
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css'],
-    standalone: false
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css'],
+  standalone: false
 })
 export class CourseComponent implements OnInit {
 
-  course: Course;
+  // Single data observable pattern.
+  courseData$: Observable<CourseData>;
 
-  lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
-
-
+  constructor(private route: ActivatedRoute,
+              private coursesService: CoursesService,
+              private loadingService: LoadingService) {
   }
 
   ngOnInit() {
+    const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'));
+    const course$ = concat(of(null), this.coursesService.loadCourseById$(courseId));
+    const lessons$ = concat(of(null), this.coursesService.loadAllCourseLessons$(courseId));
 
+    // Combine latest emitted values of two observables.
+    // [null, null] (since both observables emit null as first val)
+    // If courses is loaded faster:
+    // [courseObj, null]
+    // And when lessons are loaded:
+    // [courseObj, lessonsArr]
 
+    this.courseData$ = combineLatest([course$, lessons$]).pipe(
+      tap((x) => console.log(x)),
+      map(([course, lessons]) => ({course, lessons} as CourseData))
+    );
 
   }
 
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -33,6 +33,24 @@ export class CoursesService {
     }).pipe(
       map(res => res["payload"]),
       shareReplay()
-    )
+    );
+  }
+
+  loadCourseById$(courseId: number): Observable<Course> {
+    return this.httpClient.get<Course>(`/api/courses/${courseId}`).pipe(
+      shareReplay()
+    );
+  }
+
+  loadAllCourseLessons$(courseId: number): Observable<Lesson[]> {
+    return this.httpClient.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: '10000',
+        courseId: courseId.toString()
+      }
+    }).pipe(
+      map(res => res["payload"]),
+      shareReplay()
+    );
   }
 }
