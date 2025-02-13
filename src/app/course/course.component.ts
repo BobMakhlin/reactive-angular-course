abc202a9ae7f8193afc6b6d759f8009ecdc2917c
Makhlin.Borys <Makhlin.Borys@scheidt-bachmann.de>
2025-02-13 12:48:08 +0200

    on push

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {combineLatest, concat, Observable, of} from 'rxjs';
@@ -16,7 +16,8 @@ interface CourseData {
   selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css'],
-  standalone: false
+  standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
